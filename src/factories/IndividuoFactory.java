fcd74607b738fa2a60ec0efd6fc731e5130bb82d
Alejandro Tobías Ángeles <91081878+satse13@users.noreply.github.com>
2023-02-22 14:01:54 +0100

    Delete src directory

--- src/factories/IndividuoFactory.java
+++
@@ -1,9 +0,0 @@
-package factories;
-
-import java.util.ArrayList;
-
-import model.individuos.Individuo1;
-
-public interface IndividuoFactory<Individuo>{
-	public ArrayList<Individuo> generatePob(int tamPoblacion, double valorError);
-}
